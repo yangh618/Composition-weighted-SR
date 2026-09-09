f7692565da78900b1d3cc370b6241a91b6f52e07
Yang Huang <hyhy123.cn@gmail.com>
2026-09-09 13:45:50 +0800

    chore: bump version to 0.1.0

--- setup.py
+++ setup.py
@@ -41,7 +41,7 @@ extra_deps = [
 
 setup(
     name="cwsr",
-    version="0.0.1",
+    version="0.1.0",
     description="Composition-weighted Symbolic Regression (CWSR) with MCTS",
     long_description=long_description,
     long_description_content_type="text/markdown",
